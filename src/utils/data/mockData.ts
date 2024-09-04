6fb13faa4de9ba4aee26e1d4d55917224f394f01
Minimalist30 <kouki4122@icloud.com>
2024-09-04 17:18:53 +0900

    ディレクトリ構成

---
+++ src/utils/data/mockData.ts
@@ -0,0 +1,22 @@
+//todo: ちゃんとしたデータ入れる
+
+export const users = [
+  {
+    id: 'A',
+    name: 'ユーザーA',
+    favoriteArtists: ['アーティスト1', 'アーティスト2'],
+    favoriteSongs: [
+      { title: '曲1', artist: 'アーティスト1' },
+      { title: '曲2', artist: 'アーティスト2' },
+    ],
+  },
+  {
+    id: 'B',
+    name: 'ユーザーB',
+    favoriteArtists: ['アーティスト3', 'アーティスト4'],
+    favoriteSongs: [
+      { title: '曲3', artist: 'アーティスト3' },
+      { title: '曲4', artist: 'アーティスト4' },
+    ],
+  },
+];
